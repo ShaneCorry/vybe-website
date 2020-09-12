4694e9c611ffa5c2b462b3c6606e526689a040df
ShaneCorry <shane@corry.ie>
2020-09-12 22:55:26 +0200

    Fix hero text causing mobile breakpoint overflow

--- components/HomeHero.vue
+++ components/HomeHero.vue
@@ -3,7 +3,7 @@
     <Navigation />
     <div class="flex flex-1 items-center">
       <div class="max-w-md -mt-10">
-        <h1 class="text-3xl md:text-5xl font-black leading-snug uppercase mb-8">Vybe is modern decentralized finance</h1>
+        <h1 class="text-3xl md:text-5xl break-words font-black leading-snug uppercase mb-8">Vybe is modern decentralized finance</h1>
         <a
           href="#mission"
           class="border-4 border-black rounded-md py-4 px-12 text-lg font-bold uppercase leading-none hover:border-indigo-600"
