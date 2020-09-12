826d6cabd8544511c7fc8b10498fcf8e6ebf06b6
ShaneCorry <shane@corry.ie>
2020-09-12 16:34:45 +0200

    Add loans section

--- components/LoansSection.vue
+++ components/LoansSection.vue
@@ -1,5 +1,26 @@
 <template>
+  <section class="bg-gray-100 text-gray-900 py-16">
+    <div class="max-w-screen-xl mx-auto">
 
+      <div class="pb-10">
+        <h2 class="text-3xl font-bold mb-6">Loans</h2>
+        <p class="text-xl font-normal tracking-wide leading-relaxed max-w-2xl">Our first Dapp - Vybe Loans is a cryptographically secure way to loan VYBE, where every loan is guaranteed to be paid back in full.<br/>
+        </br/>There is no risk to any participants. When they're paid back, 1% interest is earned. This interest is burnt, directly contributing its value back to the ecosystem.</p>
+      </div>
+
+      <div class="bg-gray-200 rounded-lg shadow-sm py-8 px-10 max-w-2xl flex items-center">
+        <img src="~assets/icons/etherscan-logo.svg" alt="Etherscan Logo" class="h-8 pr-6 mr-6 border-r border-black">
+        <a
+          href="https://etherscan.io/address/0x382EE41496E0Bb88F046F2C0D1Cf894F8D272BD5"
+          target="_blank"
+          class="text-lg text-black font-bold hover:text-indigo-700"
+        >
+          View the Vybe Loans smart contract
+        </a>
+      </div>
+
+    </div>
+  </section>
 </template>
 
 <script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,6 +2,7 @@
   <div class="">
     <HomeHero />
     <MissionSection />
+    <LoansSection />
   </div>
 </template>
 
