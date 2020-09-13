b7213863a0ecfab02b10dbd63be6a1e71c4e2c4e
ShaneCorry <shane@corry.ie>
2020-09-13 13:30:37 +0200

    Desktop spacing changes

--- components/HomeHero.vue
+++ components/HomeHero.vue
@@ -1,7 +1,7 @@
 <template>
   <header class="flex flex-col max-w-screen-xl h-screen mx-auto p-8">
     <Navigation />
-    <div class="flex flex-1 flex-col justify-around items-center lg:justify-start lg:flex-row lg:grid lg:grid-cols-12 xl:grid-cols-11 lg:gap-8">
+    <div class="flex flex-1 flex-col justify-around items-center lg:justify-start lg:flex-row lg:grid lg:grid-cols-12 xl:grid-cols-10 lg:gap-8 xl:gap-0">
       <!-- <div class="relative max-w-md -mt-10"> -->
       <div class="relative max-w-md xl:max-w-none lg:col-span-6 xl:col-span-4 xl:-mt-10 text-center lg:text-left">
         <h1 class="md:mt-6 xl:mt-0 text-3xl md:text-5xl break-words font-black leading-snug uppercase mb-8">Vybe is modern decentralized finance</h1>
@@ -16,7 +16,7 @@
       <img
         src="~assets/img/vybe-hero-image-2.png"
         alt="3D Illustration"
-        class="md:mt-8 sm:w-1/2 xl:mt-0 lg:w-full lg:col-span-6 xl:col-span-7"
+        class="md:mt-8 sm:w-1/2 xl:mt-0 lg:w-full lg:col-span-6"
       />
     </div>
   </header>
