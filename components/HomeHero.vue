d26ae0fb8733a3861f0ee7f9f42bd0a51b1230d0
ShaneCorry <shane@corry.ie>
2020-09-13 13:27:42 +0200

    Added custom illustration to Hero section

--- components/HomeHero.vue
+++ components/HomeHero.vue
@@ -1,9 +1,10 @@
 <template>
   <header class="flex flex-col max-w-screen-xl h-screen mx-auto p-8">
     <Navigation />
-    <div class="flex flex-1 items-center">
-      <div class="max-w-md -mt-10">
-        <h1 class="text-3xl md:text-5xl break-words font-black leading-snug uppercase mb-8">Vybe is modern decentralized finance</h1>
+    <div class="flex flex-1 flex-col justify-around items-center lg:justify-start lg:flex-row lg:grid lg:grid-cols-12 xl:grid-cols-11 lg:gap-8">
+      <!-- <div class="relative max-w-md -mt-10"> -->
+      <div class="relative max-w-md xl:max-w-none lg:col-span-6 xl:col-span-4 xl:-mt-10 text-center lg:text-left">
+        <h1 class="md:mt-6 xl:mt-0 text-3xl md:text-5xl break-words font-black leading-snug uppercase mb-8">Vybe is modern decentralized finance</h1>
         <a
           href="#mission"
           class="border-4 border-black rounded-md py-4 px-12 text-lg font-bold uppercase leading-none hover:border-indigo-600"
@@ -12,6 +13,11 @@
           Learn More
         </a>
       </div>
+      <img
+        src="~assets/img/vybe-hero-image-2.png"
+        alt="3D Illustration"
+        class="md:mt-8 sm:w-1/2 xl:mt-0 lg:w-full lg:col-span-6 xl:col-span-7"
+      />
     </div>
   </header>
 </template>
